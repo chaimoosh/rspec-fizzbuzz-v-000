43ec1c9dc2bf0dc10748f4bdebfa772fd470b9bd
Chaim Reisman <crreisman@gmail.com>
2018-01-18 21:39:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
 if int % 3 == 0
-  "Fizz"
+ puts "Fizz"
   elseif int % 5 == 0
-    "Buzz"
+    puts "Buzz"
 elseif int % 3 == 0 && int % 5 == 0
-  "FizzBuzz"
+ puts "FizzBuzz"
 end
 
  end
